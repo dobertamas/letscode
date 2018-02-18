2b45df232dc3b775303995fd47ed9a444931474d
Tamas Dober <tamas.dober@mobileforming.com>
2018-02-17 19:34:06 -0800

    adding pytest ordering

--- hms/tests/login_tests.py
+++ hms/tests/login_tests.py
@@ -1,21 +1,27 @@
 from selenium import webdriver
 from ..pages.login_page import LoginPage
 import unittest
+import pytest
 
 
 class LoginTests(unittest.TestCase):
+    base_URL = "http://localhost:8084/console/login"
+    driver = webdriver.Firefox()
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+    driver.get(base_URL)
 
+    login_page = LoginPage(driver)
+
+    @pytest.mark.run(order=1)
     def test_valid_login(self):
-        base_URL = "http://localhost:8084/console/login"
-        driver = webdriver.Firefox()
-        driver.maximize_window()
-        driver.implicitly_wait(3)
-        driver.get(base_URL)
+        self.login_page.login("admin", "admin")
 
-        login_page = LoginPage(driver)
-        login_page.login("admin", "admin")
+        self.assertTrue(self.login_page.verifyPageTitlePresent())
+        self.assertEquals(self.driver.title, "HMS: Global Prefs")
 
-        self.assertTrue(login_page.verifyPageTitlePresent())
-        self.assertEquals(driver.title, "HMS: Global Prefs")
+        # driver.quit()
 
-        driver.quit()
+    @pytest.mark.run(order=2)
+    def test_inValid_login(self):
+        pass
